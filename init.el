eab4ece7ee1eee9a7c3b5e66b39c0d1593d050b1
pderichs <patrick@derichs.me>
2020-08-08 18:41:46 +0200

    Add alternative emacs config

--- init.el
+++ init.el
@@ -35,6 +35,38 @@
 ;; Menu bar off
 (menu-bar-mode -1)
 
+;; Save custom changes to another file
+(setq custom-file "~/.emacs.d/custom.el")
+
+;; Customize backup file creation
+;; (thanks to https://stackoverflow.com/a/20824625)
+(setq backup-directory-alist `(("." . "~/.emacs-backups")))
+
+(setq version-control t     ;; Use version numbers for backups.
+      kept-new-versions 10  ;; Number of newest versions to keep.
+      kept-old-versions 0   ;; Number of oldest versions to keep.
+      delete-old-versions t ;; Don't ask to delete excess backup versions.
+      backup-by-copying t)  ;; Copy all files, don't rename them.
+
+;; Backup versioned files as well
+(setq vc-make-backup-files t)
+
+(defun force-backup-of-buffer ()
+  ;; Make a special "per session" backup at the first save of each
+  ;; emacs session.
+  (when (not buffer-backed-up)
+    ;; Override the default parameters for per-session backups.
+    (let ((backup-directory-alist '(("" . "~/.emacs.d/backup/per-session")))
+          (kept-new-versions 3))
+      (backup-buffer)))
+  ;; Make a "per save" backup on each save.  The first save results in
+  ;; both a per-session and a per-save backup, to keep the numbering
+  ;; of per-save backups consistent.
+  (let ((buffer-backed-up nil))
+    (backup-buffer)))
+
+(add-hook 'before-save-hook  'force-backup-of-buffer)
+
 ;; Line numbers
 (when (version<= "26.0.50" emacs-version )
   (global-display-line-numbers-mode))
@@ -76,20 +108,8 @@
 (global-set-key (kbd "<f8>") 'magit)
 ;; Find definition(s)
 (global-set-key (kbd "<f12>") 'dumb-jump-go)
+;; Find word under cursor
+(global-set-key (kbd "<f4>") 'swiper-thing-at-point)
 
 ;; TODO: Split window?
 ;; TODO: Other window?
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (magit swiper swiper-helm helm helm-rg iedit dumb-jump))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
