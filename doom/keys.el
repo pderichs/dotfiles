266582bea86d94e81a78d7029761cccb1fc1a3cc
pderichs <derichs@posteo.de>
2023-09-25 15:38:52 +0200

    Add neotree-toggle

--- doom/keys.el
+++ doom/keys.el
@@ -41,6 +41,7 @@
 (map! :leader :desc "Open Zettelkasten search" "ze" (lambda () (interactive) (consult-ripgrep (concat user-personal-zettelkasten-folder))))
 (map! :leader :desc "Open current folder in file manager" "cd" (lambda () (interactive) (call-process "xdg-open" nil 0 nil ".")))
 (map! :leader :desc "Search for definition in another window" "gow" #'xref-find-definitions-other-window)
+(map! :leader :desc "Toggle neotree" "nt" #'neotree-toggle)
 
 ;; Switch view of workspace buffers with all buffers
 (map! :leader :desc "Quick jump to all buffers" "bb" #'consult-buffer)
