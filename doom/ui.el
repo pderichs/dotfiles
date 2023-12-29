446df55c200d18f5416694c0fb3a2620cf86f75d
pderichs <derichs@posteo.de>
2023-12-29 11:03:04 +0100

    Update doom config: Add customized adwaita theme

--- doom/ui.el
+++ doom/ui.el
@@ -28,10 +28,13 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq pd/ui-theme 'adwaita)
+(setq pd/ui-theme 'pd-adwaita)
 (if (string= (getenv "PD_LAPTOP") "1")
     (setq doom-theme pd/ui-theme)
   (setq doom-theme pd/ui-theme))
+
+;; ;; adwaita: enable syntax highlighting in current line
+(set-face-foreground 'highlight nil)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
