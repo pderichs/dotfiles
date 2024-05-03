cdd09e97640608cf3f4d796e1e22a6d954c38d75
pderichs <derichs@posteo.de>
2024-05-03 07:43:38 +0200

    Update neovim config

--- neovim/init.lua
+++ neovim/init.lua
@@ -261,23 +261,8 @@ if status_ok then
     'rbgrouleff/bclose.vim', -- required by ranger
     {
       'nvim-orgmode/orgmode',
-      dependencies = {
-        { 'nvim-treesitter/nvim-treesitter', lazy = true },
-      },
       event = 'VeryLazy',
       config = function()
-        -- Load treesitter grammar for org
-        require('orgmode').setup_ts_grammar()
-
-        -- Setup treesitter
-        require('nvim-treesitter.configs').setup({
-          highlight = {
-            enable = true,
-            additional_vim_regex_highlighting = { 'org' },
-          },
-          ensure_installed = { 'org' },
-        })
-
         -- Setup orgmode
         require('orgmode').setup({
           org_agenda_files = '~/orgfiles/**/*',
