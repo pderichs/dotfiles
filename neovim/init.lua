afd95a0e9b1b8ee2fa42835706bd0c2dc11e0571
pderichs <derichs@posteo.de>
2024-07-14 08:47:22 +0200

    Update neovim config

--- neovim/init.lua
+++ neovim/init.lua
@@ -297,4 +297,5 @@ vim.g.ranger_command_override = 'ranger --cmd "set show_hidden=true"'
 vim.g.ranger_map_keys = 0 -- prevent ranger plugin from updating keys
 
 -- vim.opt.background = 'dark'
-vim.cmd.colorscheme('allure-contrast')
+-- vim.cmd.colorscheme('allure-contrast')
+vim.cmd.colorscheme('banner')
