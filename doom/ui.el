bc1a66d627f68e0b04a7960365ad48c28079cfe2
pderichs <derichs@posteo.de>
2024-08-26 12:21:32 +0200

    Update doom config

--- doom/ui.el
+++ doom/ui.el
@@ -34,7 +34,7 @@
 ;; (if (>= pd/current-hour 18)
 ;;     (setq pd/ui-theme 'doom-city-lights)
 ;;   (setq pd/ui-theme 'pd-adwaita))
-(setq pd/ui-theme 'doom-solarized-dark-high-contrast)
+(setq pd/ui-theme 'doom-acario-light)
 
 ;; (if (string= (getenv "PD_LAPTOP") "1")
 ;;     (setq doom-theme pd/ui-theme)
