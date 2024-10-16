c1c40c95a2163dd81d023591c273eb28569b0cc7
pderichs <derichs@posteo.de>
2024-10-16 21:59:53 +0200

    Update neovim config
    Setup LSP

--- neovim/init.lua
+++ neovim/init.lua
@@ -63,6 +63,9 @@ vim.opt.path:append { '**' } -- Finding files - Search down into subfolders
 vim.opt.wildignore:append { '*/node_modules/*' }
 vim.opt.relativenumber = true
 
+-- Use system clipboard
+vim.opt.clipboard = "unnamedplus"
+
 -- Always change directory to the current buffer location
 -- see: https://vim.fandom.com/wiki/Set_working_directory_to_the_current_file
 vim.cmd('autocmd BufEnter * silent! lcd %:p:h')
@@ -241,12 +244,25 @@ vim.opt.rtp:prepend(lazypath)
 local status_ok, lazy = pcall(require, "lazy")
 if status_ok then
   lazy.setup({
-    'neovim/nvim-lspconfig',
     'folke/which-key.nvim',
     'aymericbeaumet/vim-symlink',
     'nvim-lua/plenary.nvim',
     'terrortylor/nvim-comment',
     { 'nvim-telescope/telescope.nvim', tag = '0.1.5' },
+    -- LSP START
+    'neovim/nvim-lspconfig',
+    'williamboman/mason.nvim',
+    'williamboman/mason-lspconfig.nvim',
+
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+    'hrsh7th/cmp-cmdline',
+    'hrsh7th/nvim-cmp',
+    'hrsh7th/cmp-vsnip',
+    'hrsh7th/vim-vsnip',
+
+    -- LSP END
     'folke/zen-mode.nvim',
     'folke/neodev.nvim',
     'nvim-treesitter/nvim-treesitter',
@@ -277,6 +293,111 @@ if status_ok then
 else
   print("Unable to load lazy.nvim")
 end
+
+--------------------------------------------------------------------------------------
+-- LSP Setup
+
+local language_servers = { "pyright", "ts_ls" }
+
+local status_ok, mason = pcall(require, 'mason')
+if status_ok then
+  mason.setup()
+else
+  print('Unable to load mason')
+end
+
+local status_ok, mason_lspconfig = pcall(require, 'mason-lspconfig')
+if status_ok then
+  mason_lspconfig.setup({
+    ensure_installed = language_servers
+  })
+else
+  print('Unable to load mason-lspconfig')
+end
+
+local status_ok, lspconfig = pcall(require, 'lspconfig')
+if status_ok then
+  lspconfig.pyright.setup({})
+else
+  print('Unable to load lspconfig')
+end
+
+local cmp = require'cmp'
+
+cmp.setup({
+  snippet = {
+    -- REQUIRED - you must specify a snippet engine
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+      -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+      -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+      -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+    end,
+  },
+  window = {
+    -- completion = cmp.config.window.bordered(),
+    -- documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' }, -- For vsnip users.
+    -- { name = 'luasnip' }, -- For luasnip users.
+    -- { name = 'ultisnips' }, -- For ultisnips users.
+    -- { name = 'snippy' }, -- For snippy users.
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+-- To use git you need to install the plugin petertriho/cmp-git and uncomment lines below
+-- Set configuration for specific filetype.
+--[[ cmp.setup.filetype('gitcommit', {
+  sources = cmp.config.sources({
+    { name = 'git' },
+  }, {
+    { name = 'buffer' },
+  })
+})
+require("cmp_git").setup() ]]-- 
+
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  }),
+  matching = { disallow_symbol_nonprefix_matching = false }
+})
+
+-- Set up lspconfig.
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+-- Set capabilities for each installed language server
+for i, value in ipairs(language_servers) do
+  require('lspconfig')[value].setup {
+    capabilities = capabilities
+  }
+end
+
+--------------------------------------------------------------------------------------
 
 -- thanks to https://github.com/LunarVim/Neovim-from-scratch
 -- We use a protected require so we don't end up in an error on 
