883466ae262ce3d30eadcc9034362ec2ba16c094
pderichs <derichs@posteo.de>
2025-11-21 09:33:27 +0100

    Update roam config

--- doom/roam.el
+++ doom/roam.el
@@ -3,13 +3,6 @@
   :custom
   (org-roam-directory (file-truename "~/roam"))
   (org-roam-db-location "~/roam/org-roam.db")
-  :bind (("C-c n l" . org-roam-buffer-toggle)
-         ("C-c n f" . org-roam-node-find)
-         ("C-c n g" . org-roam-graph)
-         ("C-c n i" . org-roam-node-insert)
-         ("C-c n c" . org-roam-capture)
-         ;; Dailies
-         ("C-c n j" . org-roam-dailies-capture-today))
   :config
   ;; If you're using a vertical completion framework, you might want a more informative completion interface
   (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
