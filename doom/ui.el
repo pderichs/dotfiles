b523fc961cf205aa2ec0844258c8f00e6e8a1997
pderichs <patrick@derichs.me>
2026-02-04 14:28:22 +0100

    Update doom config

--- doom/ui.el
+++ doom/ui.el
@@ -38,7 +38,7 @@
 ;; (if (>= pd/current-hour 18)
 ;;     (setq pd/ui-theme 'doom-city-lights)
 ;;   (setq pd/ui-theme 'pd-adwaita))
-(setq pd/ui-theme 'sanityinc-tomorrow-blue)
+(setq pd/ui-theme 'doom-moonlight)
 
 ;; (if (string= (getenv "PD_LAPTOP") "1")
 ;;     (setq doom-theme pd/ui-theme)
